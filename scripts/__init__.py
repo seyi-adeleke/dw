21c95013937bc5ff4945e8ac4cf48af0a0b9cdbf
seyi-adeleke <seyiadeleke42@yahoo.com>
2020-01-13 22:09:11 +0100

    add csv files

--- scripts/__init__.py
+++ scripts/__init__.py
@@ -1,26 +1,31 @@
-import requests
 import xlrd
-import csv
 from urllib.parse import urljoin
-from utils import url_is_valid, make_request, parse_sheet, write_csv, get_filename
+from utils import url_is_valid, make_request, parse_sheet, write_csv, get_filename, convert_to_soup
 
 url_dict = {
     "day": "https://www.eia.gov/dnav/ng/hist/rngwhhdD.htm",
     "month": "https://www.eia.gov/dnav/ng/hist/rngwhhdM.htm"
     }
 
+xl_sheet_name = 'Data 1'
+
 def main():
     for key in url_dict:
         url = url_dict[key]
         if (url_is_valid(url_dict[key])):
-            soup_data = make_request(url)
+            html_data = make_request(url)
+            soup_data = convert_to_soup(html_data)
+
             link_to_xls_file = soup_data.select_one("[href*='hist_xls']")
-            xls_response = requests.get(urljoin(url, link_to_xls_file["href"]))
-            workbook = xlrd.open_workbook(file_contents=xls_response.content)
-            xl_sheet = workbook.sheet_by_name("Data 1")
+            xl_data = make_request(urljoin(url, link_to_xls_file["href"]))
+            xl_workbook = xlrd.open_workbook(file_contents=xl_data.content)
+            xl_sheet = xl_workbook.sheet_by_name(xl_sheet_name)
+
             parsed_data = parse_sheet(xl_sheet)
-            filename = parse_filename(key)
+            filename = get_filename(key)
+
             write_csv(parsed_data, filename)
+            print("Created CSV File: " + filename)
         else:
             print("Url Passed is not valid")
 
--- scripts/utils.py
+++ scripts/utils.py
@@ -1,5 +1,7 @@
 import requests
 import csv
+import xlrd
+
 from urllib.parse import urlparse
 from bs4 import BeautifulSoup
 
@@ -12,14 +14,17 @@ def url_is_valid(url):
 
 def make_request(url):
     response = requests.get(url)
-    return BeautifulSoup(response.content, features="html.parser")
+    return response
+
+def convert_to_soup(data):
+    return BeautifulSoup(data.content, features="html.parser")
 
 def parse_sheet(xl_sheet):
     data = []
     for row in xl_sheet.get_rows():
         if(row[0].ctype == 3):
-            date = xlrd.xldate_as_datetime(row[0].value, 0).date().isoformat()
             price = row[1].value
+            date = xlrd.xldate_as_datetime(row[0].value, 0).date().isoformat()
             data.append([date, price])
     return data
 
